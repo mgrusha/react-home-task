2f56c55dc820686ad848f0abfc71e0db1389da1f
mykytagrusha <mykyta.grusha@collibra.com>
2020-05-23 00:58:41 +0200

    Updates

---
+++ precache-manifest.7ebf92276281b401ca1283663ce25638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df362d5809c931ea81dc7bf0455006b2",
+    "url": "/react-home-task/index.html"
+  },
+  {
+    "revision": "5a2b63263077cb005c8f",
+    "url": "/react-home-task/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "81d7c8e22a4064ceb6e6",
+    "url": "/react-home-task/static/js/2.205e800b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-home-task/static/js/2.205e800b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2b63263077cb005c8f",
+    "url": "/react-home-task/static/js/main.e2cae78f.chunk.js"
+  },
+  {
+    "revision": "44312ec885323a2e2846",
+    "url": "/react-home-task/static/js/runtime-main.f771ff23.js"
+  }
+]);
